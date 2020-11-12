4dd5eac8bd0d09fd2ec2d6d5981f3b23da6d1d3c
mora5925 <wahba190@gmail.com>
2020-11-12 12:46:24 -0500

    setting Api

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  render() {
+const App = () => {
+
+  const APP_ID = "48a1943a";
+  const APP_KEY = "4d91ea04b25be1b008370b4b06638440";
+
+
+
     return (
       <div className="App">
        <h1>Food Recipes</h1>
       </div>
     );
-  }
-}
+  };
+
 
 export default App;
